71e05e7b3c30b1b9522d3891c75f910c553d5a9c
YT C <qq308596148@gmail.com>
2024-06-03 19:06:30 -0400

    Add Teris level and stuff
    Tetris levels, assets, as well as the colors and shapes of blocks through the program. After completing a level, the program will output the number of blocks corresponding to each specific color.

---
+++ Assets/Teris/Scripts/SpawnTetromino.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetromino : MonoBehaviour
+{
+    public GameObject[] Tetrominoes;
+    public Color[] Colors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetromino();
+    }
+
+    public void NewTetromino()
+    {
+        // Instantiate a random Tetromino
+        GameObject NewTetromino = Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+        ApplyRandomColor(NewTetromino);
+    }
+
+    public void ApplyRandomColor(GameObject tetromino)
+    {
+        // Choose a random color from the Colors array
+        Color randomColor = Colors[Random.Range(0, Colors.Length)];
+
+        // Get all Renderer components in the Tetromino and its children
+        Renderer[] renderers = tetromino.GetComponentsInChildren<Renderer>();
+
+        // Apply the random color to each Renderer
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material.color = randomColor;
+        }
+    }
+}
+
+
---
+++ Assets/Teris/Scripts/TetrisBlock.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    public Vector3 rotationPoint;
+    private float previousTime;
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+    private static Transform[,] grid = new Transform[width, height];
+
+    private SpawnTetromino spawnTetromino;
+
+    private static Dictionary<string, int> globalColorCount = new Dictionary<string, int>(); // Create a global dictionary to count colors
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTetromino = FindObjectOfType<SpawnTetromino>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // Rotate
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            if (!ValidMove())
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+        }
+
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                CheckForLines();
+                this.enabled = false;
+                FindObjectOfType<SpawnTetromino>().NewTetromino();
+            }
+            previousTime = Time.time;
+        }
+    }
+
+    void CheckForLines()
+    {
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+
+        // Output the deleted color count after checking all lines
+        if (globalColorCount.Count > 0)
+        {
+            string result = "Eliminated Colors: ";  // Eliminated Colors: #00FF07FF Count: 15; #0010FFFF Count: 1; #FF0000FF Count: 4; 
+                                                    // We can decide the attack method based on this parameter.
+
+            foreach (var entry in globalColorCount)
+            {
+                result += $"#{entry.Key} Count: {entry.Value}; ";
+            }
+            Debug.Log(result);
+            globalColorCount.Clear(); // Clear the global dictionary for the next set of eliminations
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] != null)
+            {
+                Renderer renderer = grid[j, i].GetComponent<Renderer>();
+                string colorCode = ColorUtility.ToHtmlStringRGBA(renderer.material.color);
+
+                if (globalColorCount.ContainsKey(colorCode))
+                {
+                    globalColorCount[colorCode]++;
+                }
+                else
+                {
+                    globalColorCount[colorCode] = 1;
+                }
+
+                Destroy(grid[j, i].gameObject);
+                grid[j, i] = null;
+            }
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+
+            if (grid[roundedX, roundedY] != null)
+                return false;
+        }
+        return true;
+    }
+}
+
