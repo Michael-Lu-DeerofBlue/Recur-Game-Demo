ab89816dfb8583636b862bff3de031227cee685d
Joe Lu <ylu4@swarthmore.edu>
2024-06-08 17:24:56 -0400

    updared

---
+++ Assets/Interactables/TestInteractable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteractable : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void Interact()
+    {
+        Debug.Log("Interacted with" + gameObject.name);
+    }
+}
---
+++ Assets/Player/Interactable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    //message displayed to player when looking at an interactable
+    public string promptMessage;
+    //this function will be called from our player.
+    public void BaseInteract()
+    {
+        Interact();
+    }
+
+    // Update is called once per frame
+    protected virtual void Interact()
+    {
+        
+    }
+}
