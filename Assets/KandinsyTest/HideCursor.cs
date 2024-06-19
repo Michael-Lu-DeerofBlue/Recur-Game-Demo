4981aa1e51443efcde6f85009b6da3e33d6df327
niallfw <ferriswaynen@go.oes.edu>
2024-06-19 11:10:38 -0700

    kandinsky testing

---
+++ Assets/KandinsyTest/HideCursor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class CursorScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+}
