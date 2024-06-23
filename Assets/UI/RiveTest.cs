be73398e813d90530172e26966736fe1672a5880
Joe Lu <ylu4@swarthmore.edu>
2024-06-22 21:37:22 -0400

    michael

---
+++ Assets/UI/RiveTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiveTest : MonoBehaviour
+{
+    /*
+    public Rive.Asset asset; // pass in .riv asset in the inspector
+    private Rive.File m_file;
+
+
+private void Start()
+    {
+        if (asset != null)
+        {
+            m_file = Rive.File.Load(asset);
+        }
+    }
+    */
+}
