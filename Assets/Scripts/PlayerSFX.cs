5a9241afbd3e86097bcfe9026eacfaab59b2e159
niallfw <ferriswaynen@go.oes.edu>
2024-07-08 16:28:38 +1200

    player walking sfx

--- Assets/Scripts/PlayerSFX.cs
+++ Assets/Scripts/PlayerSFX.cs
@@ -1,49 +1,69 @@
-// using Kamgam.SettingsGenerator;
-// using Unity.VisualScripting;
+using Unity.VisualScripting;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using Fungus;
+using Fungus;
 
 public class PlayerSFX : MonoBehaviour
 {
-    // public playerController Controller;
-    // public Flowchart flowchart; // Reference to the Fungus Flowchart
-    // public bool movementSpeed = Controller.movementSpeed;
-    // public bool inAir = Controller.inAir;
-    // public bool isGrounded = Controller.isGrounded;
+    PlayerController playerController;
+    [SerializeField] GameObject player;
+    public Flowchart flowchart; // Reference to the Fungus Flowchart
+    private bool wasWalking = false; // Track the previous movement state
 
-    void Start()
+    void Awake()
     {
-        
+        playerController = player.GetComponent<PlayerController>();
     }
-    // void Update()
-    // {
-    //     if (Controller == null)
-    //     {
-    //         Debug.LogError("PlayerController reference is missing.");
-    //         return;
-    //     }
-        
-    //     //get player input
-    //     float moveHorizontal = Input.GetAxis("Horizontal");
-    //     float moveVertical = Input.GetAxis("Vertical");
-        
-    //     //check if player is walking
-    //     if ((moveHorizontal != 0 || moveVertical != 0) && inAir == false && isGrounded == true)
-    //     {
-    //         PlayEffectWalk();
-    //     }        
 
-    // }
+    void Update()
+    {
+        if (playerController == null)
+        {
+            Debug.LogError("PlayerController reference is missing.");
+            return;
+        }
+        
+        // Get player input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-    // void PlayEffectWalk()
-    // {
-    //     // Check if the flowchart is not already executing
-    //     if (!flowchart.HasExecutingBlocks())
-    //     {
-    //         // Start the Fungus flowchart
-    //         flowchart.ExecuteBlock("PlayerWalk"); // Replace "Start" with the name of your block
-    //     }
-    // }
+        //float movementSpeed = playerController.movementSpeed;
+        bool inAir = playerController.inAir;
+        bool isGrounded = playerController.isGrounded;
+
+        // Check if player is walking
+        bool isWalking = (moveHorizontal != 0 || moveVertical != 0) && !inAir && isGrounded;
+
+        if (isWalking && !wasWalking)
+        {
+            // Player has started walking
+            PlayEffectWalk();
+        }
+        else if (!isWalking && wasWalking)
+        {
+            // Player has stopped walking
+            StopEffectWalk();
+        }
+
+        // Update the previous movement state
+        wasWalking = isWalking;
+    }
+
+    void PlayEffectWalk()
+    {
+        // Check if the flowchart is not already executing
+        if (!flowchart.HasExecutingBlocks())
+        {
+            // Start the Fungus flowchart
+            flowchart.ExecuteBlock("PlayerWalk"); // Replace "PlayerWalk" with the name of your block
+        }
+    }
+
+    void StopEffectWalk()
+    {
+        // Stop the Fungus flowchart or any other logic to stop the walking sound
+        // For example, you can call another block in Fungus to stop the sound
+        flowchart.ExecuteBlock("StopPlayerWalk"); // Replace "StopPlayerWalk" with the name of your block that stops the sound
+    }
 }
