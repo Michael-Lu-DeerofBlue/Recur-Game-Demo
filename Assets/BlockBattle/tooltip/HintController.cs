a2cd2f78f823b8b13539fbd7f07cac1762929fda
FrenziiMusic <frenzii.rec@gmail.com>
2024-07-24 16:16:04 +0800

    Central Meditation Room Music Implementation

--- Assets/BlockBattle/tooltip/HintController.cs
+++ Assets/BlockBattle/tooltip/HintController.cs
@@ -18,6 +18,11 @@ public class HintController : MonoBehaviour
     public TextMeshPro countText;
     public Flowchart flowchart;
     public GameObject perTutorialManager;
+    public AK.Wwise.Event PlayCorridorLoop;
+    public AK.Wwise.Event PlayTutorialLoop;
+    public AK.Wwise.State Non_Battle;
+    public AK.Wwise.State Battle;
+    public AK.Wwise.Event PerseusClear;
     void Start()
     {
         // Ensure all game objects are disabled except the first one
@@ -25,108 +30,113 @@ public class HintController : MonoBehaviour
         {
             gameObjects[i].SetActive(i == 0);
         }
-    }
-
-    private void Update()
-    {
         string currentSceneName = SceneManager.GetActiveScene().name;
         if (currentSceneName == "BattleLevel - tutorial")
         {
-            countText.text = lineCleared.ToString() + "/5";
+            PerseusClear.Post(gameObject);
+            PlayTutorialLoop.Post(gameObject);
         }
     }
-
-    public void ExitOut()
-    {
-        JudgeLanguage();
-        DialogueManager.StartConversation(conversationName + "_" + language);
-        flowchart.ExecuteBlock("WhiteScreen");
-    }
-
-    void JudgeLanguage()
-    {
-        switch (LocalizationManager.CurrentLanguage)
+        private void Update()
         {
-            case "English":
-                language = "en";
-                //set subtitle speed
-                DialogueManager.displaySettings.subtitleSettings.subtitleCharsPerSecond = 10;
-                break;
-            case "Chinese (Simplified)":
-                language = "cn";
-                //set subtitle speed
-                DialogueManager.displaySettings.subtitleSettings.subtitleCharsPerSecond = 4;
-                break;
-            // Add more cases for other languages if needed
-            default:
-                language = "en";
-                break;
-        }
-    }
-
-    public void WaitAndSwitch()
-    {
-        flowchart.ExecuteBlock("WaitAndSwitch");
-    }
-
-
-    public void SwitchTip()
-    {
-        if (gameObjects == null || gameObjects.Count == 0)
-            return;
-        //Debug.Log(gameObjects.Count);
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        //Per tutorial
-        if (currentSceneName == "BattleLevel - per - tutorial")
-        {
-            if (currentIndex < gameObjects.Count - 1)
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            if (currentSceneName == "BattleLevel - tutorial")
             {
-                // Disable the current game object
-                gameObjects[currentIndex].SetActive(false);
-               
-                // Increment the index and wrap around if needed
-                currentIndex = (currentIndex + 1);
-                if (currentIndex == 2)
+                countText.text = lineCleared.ToString() + "/5";
+            }
+        }
+
+        public void ExitOut()
+        {
+            JudgeLanguage();
+            DialogueManager.StartConversation(conversationName + "_" + language);
+            flowchart.ExecuteBlock("WhiteScreen");
+        }
+
+        void JudgeLanguage()
+        {
+            switch (LocalizationManager.CurrentLanguage)
+            {
+                case "English":
+                    language = "en";
+                    //set subtitle speed
+                    DialogueManager.displaySettings.subtitleSettings.subtitleCharsPerSecond = 10;
+                    break;
+                case "Chinese (Simplified)":
+                    language = "cn";
+                    //set subtitle speed
+                    DialogueManager.displaySettings.subtitleSettings.subtitleCharsPerSecond = 4;
+                    break;
+                // Add more cases for other languages if needed
+                default:
+                    language = "en";
+                    break;
+            }
+        }
+
+        public void WaitAndSwitch()
+        {
+            flowchart.ExecuteBlock("WaitAndSwitch");
+        }
+
+
+        public void SwitchTip()
+        {
+            if (gameObjects == null || gameObjects.Count == 0)
+                return;
+            //Debug.Log(gameObjects.Count);
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            //Per tutorial
+            if (currentSceneName == "BattleLevel - per - tutorial")
+            {
+                if (currentIndex < gameObjects.Count - 1)
                 {
-                    perTutorialManager.GetComponent<PerseusTutorial>().Resume();
-                    flowchart.ExecuteBlock("WaitAndPause");
+                    // Disable the current game object
+                    gameObjects[currentIndex].SetActive(false);
+
+                    // Increment the index and wrap around if needed
+                    currentIndex = (currentIndex + 1);
+                    if (currentIndex == 2)
+                    {
+                        perTutorialManager.GetComponent<PerseusTutorial>().Resume();
+                        flowchart.ExecuteBlock("WaitAndPause");
+                    }
+                    // Enable the next game object
+                    gameObjects[currentIndex].SetActive(true);
                 }
-                // Enable the next game object
-                gameObjects[currentIndex].SetActive(true);
-            }
-            else
-            {
-                foreach (GameObject go in gameObjects) { go.SetActive(false); }
-            }
-        }
-
-
-
-        //Tutorial
-        if (currentSceneName == "BattleLevel - tutorial")
-        {
-            if (currentIndex < gameObjects.Count - 1)
-            {
-                // Disable the current game object
-                gameObjects[currentIndex].SetActive(false);
-
-                // Increment the index and wrap around if needed
-                currentIndex = (currentIndex + 1);
-                if (currentIndex == 7)
+                else
                 {
-                    countText.gameObject.SetActive(true);
+                    foreach (GameObject go in gameObjects) { go.SetActive(false); }
                 }
-
-                // Enable the next game object
-                gameObjects[currentIndex].SetActive(true);
             }
-            else
+
+
+
+            //Tutorial
+            if (currentSceneName == "BattleLevel - tutorial")
             {
-                foreach (GameObject go in gameObjects) { go.SetActive(false); }
-            }
-        }
+                if (currentIndex < gameObjects.Count - 1)
+                {
+                    // Disable the current game object
+                    gameObjects[currentIndex].SetActive(false);
 
-       
-        
-    }
+                    // Increment the index and wrap around if needed
+                    currentIndex = (currentIndex + 1);
+                    if (currentIndex == 7)
+                    {
+                        countText.gameObject.SetActive(true);
+                    }
+
+                    // Enable the next game object
+                    gameObjects[currentIndex].SetActive(true);
+                }
+                else
+                {
+                    foreach (GameObject go in gameObjects) { go.SetActive(false); }
+                }
+            }
+
+
+
+        }
 }
