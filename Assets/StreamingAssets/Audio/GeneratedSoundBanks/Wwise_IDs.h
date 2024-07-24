fc0bc1b75dfac8b418b25a2c6cad298c398cdf57
FrenziiMusic <frenzii.rec@gmail.com>
2024-07-24 13:45:37 +0800

    Central Meditation Room Soundbank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PERSEUSCLEAR = 2507326437U;
+        static const AkUniqueID PLAYCORRIDORLOOP = 583736263U;
+        static const AkUniqueID PLAYTUTORIALLOOP = 137349311U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ENEMY_BATTLE
+        {
+            static const AkUniqueID GROUP = 324451396U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BATTLE = 2937832959U;
+                static const AkUniqueID NON_BATTLE = 3512553307U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace ENEMY_BATTLE
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
