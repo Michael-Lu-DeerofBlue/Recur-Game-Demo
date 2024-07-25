174f572cf1e6eb6fa502bd33e8af1fab1cdbb6b6
Joe Lu <ylu4@swarthmore.edu>
2024-07-25 09:06:23 +0800

    fixing

--- Assets/BlockBattle/Scripts/Enemy/Daedalus1.cs
+++ Assets/BlockBattle/Scripts/Enemy/Daedalus1.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 
 public class Daedalus1 : Enemy
 {
-
+    public Sprite WaxSprite;
     public int attackDamage = 10;
     public int WaxedWingsDamage = 16;
-    public int WaxSPrayDamage = 15;
-    public int KindledStrike = 20;
+    public int WaxSprayDamage = 15;
+    public int KindledStrikeDamage = 20;
+    public int KindledStrikeEachCubeDamage = 5;
 
     public int attackWeight = 25;
     public int WaxedWingsWeight = 40;
@@ -24,6 +25,8 @@ public class Daedalus1 : Enemy
     public float kindledStrikeCastingTime = 20;
     public float ThelabyrinthCastingTime = 5;
 
+    
+
     private enum SkillType { Attack, WaxedWings, WaxSPray, kindledStrike, Thelabyrinth }
     private SkillType nextSkill;
 
@@ -35,51 +38,156 @@ public class Daedalus1 : Enemy
                 AttackScaleAnimation(0.2f, 1.3f, 0.6f, 1.0f, attackDamage);
                 break;
             case SkillType.WaxedWings:
-
+                WaxWings();
                 break;
             case SkillType.WaxSPray:
-
+                WaxSpray();
+                DealAttackDamage(WaxSprayDamage);
                 break;
-            case SkillType.kindledStrike:
+        }
+    }
+    public void KindledStrike()
+    {
+        battleManager.KindledStrike(KindledStrikeDamage, KindledStrikeEachCubeDamage);
+        DealAttackDamage(KindledStrikeDamage);
 
+<<<<<<< Updated upstream
                 break;
             case SkillType.Thelabyrinth:
                 //TwoDto3D.
                 break;
+=======
+    }
+    public void WaxSpray()
+    {
+>>>>>>> Stashed changes
 
+            int times = 12;
+
+            while (times > 0)
+            {
+                float randomValue = Random.value;
+
+                if (randomValue < 0.1f) // 20% probability to call WaxWings()
+                {
+                    WaxWings();
+                    times -= 4;
+                }
+                else // 80% probability to call X event
+                {
+                   if(WaxRandomBlockOnGrid(WaxSprite)== true)
+                     {
+                         times -= 1;
+                     }
+                }
+            }
+    }
+
+    public bool WaxRandomBlockOnGrid(Sprite WaxSprite)
+    {
+        BlockManager blockManager = FindObjectOfType<BlockManager>();
+         bool Success= blockManager.AddSpriteToRandomBlock(WaxSprite);
+        return Success;
+
+    }
+
+
+    public void WaxWings()
+    {
+        InSelectionBar[] allBlocks = FindObjectsOfType<InSelectionBar>();
+        List<InSelectionBar> activeBlocks = new List<InSelectionBar>();
+
+        // Filter only active InSelectionBar scripts without existing "WaxSpriteChild"
+        foreach (InSelectionBar block in allBlocks)
+        {
+            if (block.isActiveAndEnabled && !HasWaxSpriteChild(block))
+            {
+                activeBlocks.Add(block);
+            }
+        }
+
+        if (activeBlocks.Count == 0)
+        {
+            Debug.Log("No active blocks found in the selection bar.");
+        }
+        else
+        {
+            // Randomly select one active block
+            InSelectionBar selectedBlock = activeBlocks[Random.Range(0, activeBlocks.Count)];
+            SpriteRenderer[] squares = selectedBlock.GetComponentsInChildren<SpriteRenderer>();
+
+            if (squares.Length == 0)
+            {
+                Debug.Log("No Square components found in the selected block.");
+            }
+            else
+            {
+                foreach (SpriteRenderer square in squares)
+                {
+                    // Add a new child game object with a SpriteRenderer component to each square
+                    GameObject newChild = new GameObject("WaxSpriteChild");
+                    newChild.transform.parent = square.transform;
+                    newChild.transform.localPosition = Vector3.zero; // Ensure the local position is (0, 0, 0)
+                    SpriteRenderer spriteRenderer = newChild.AddComponent<SpriteRenderer>();
+                    spriteRenderer.sprite = WaxSprite;
+                    spriteRenderer.sortingOrder = 12;
+                }
+            }
         }
     }
 
-    //public override void GetNextMove()
-    //{
-    //    int sum = attackWeight + ChargeWeight + GoldenAntlerWeight;
-    //    float attackProbability = (float)attackWeight / sum;
-    //    float ChargeProbability = (float)ChargeWeight / sum;
-    //    float randomValue = Random.value;
+    private bool HasWaxSpriteChild(InSelectionBar block)
+    {
+        foreach (Transform child in block.transform)
+        {
+            foreach (Transform grandchild in child)
+            {
+                if (grandchild.name == "WaxSpriteChild")
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public override void GetNextMove()
+    {
+        int sum = attackWeight + WaxedWingsWeight + WaxSPrayWeight;
+        float attackProbability = (float)attackWeight / sum;
+        float WaxedWingsProbability = (float)WaxedWingsWeight / sum;
+        float WaxSPrayProbability = (float)WaxSPrayWeight / sum;
+
+        float randomValue = Random.value;
 
 
-    //    if (randomValue < attackProbability)
-    //    {
-    //        SkillCastingTime = attackCastingTime;  // Attack action
-    //        nextSkill = SkillType.Attack;
-    //        CurrentSkillIcons = new string[] { "Damage" };
-    //        NextSkillDamage = attackDamage;
+        if (randomValue < attackProbability)
+        {
+            SkillCastingTime = attackCastingTime;  // Attack action
+            nextSkill = SkillType.Attack;
+            CurrentSkillIcons = new string[] { "Damage" };
+            NextSkillDamage = attackDamage;
 
-    //    }
-    //    else if (randomValue < attackProbability + ChargeProbability)
-    //    {
-    //        SkillCastingTime = ChargeCastingTime;
-    //        nextSkill = SkillType.Charge;
-    //        CurrentSkillIcons = new string[] { "Damage", "Buff" };
+        }
+        else if (randomValue < attackProbability + WaxedWingsProbability)
+        {
+            SkillCastingTime = WaxedWingsCastingTime;
+            nextSkill = SkillType.WaxedWings;
+            CurrentSkillIcons = new string[] { "Interrupt", "Damage" };
 
-    //    }
-    //    else
-    //    {
-    //        SkillCastingTime = GoldenAntlerCastingTime;
-    //        nextSkill = SkillType.GoldenAntler;
-    //        CurrentSkillIcons = new string[] { "Damage", "Heal" };
+        }
+        else
+        {
+            SkillCastingTime = WaxSPrayCastingTime;
+            nextSkill = SkillType.WaxSPray;
+            CurrentSkillIcons = new string[] { "Interrupt", "Damage" };
 
-    //    }
-    //    nextMove = nextSkill.ToString();
-    //}
+        }
+        nextMove = nextSkill.ToString();
+    }
+    public override void Start()
+    {
+        base.Start();
+        StaggerReis = 3;
+    }
 }
